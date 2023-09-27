478aa676c5da378ff8e92f03f060e81d6ff2e216
bluebird089 <bluebird089@proton.me>
2023-09-27 23:35:30 +0600

    added minor change

--- src/components/ErrorElement/ErrorElement.jsx
+++ src/components/ErrorElement/ErrorElement.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const ErrorElement = () => {
     return (
-        <div className="h-screen text-center">
+        <div className="h-screen text-center flex flex-col justify-center w-full gap-2">
             <h3 className="font-bold text-5xl">Oops!</h3>
             <h4 className="font-semibold text-4xl">404 Not Found</h4>
             <Link to="/"><button className="btn">Back to Home</button></Link>
